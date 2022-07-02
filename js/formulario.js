5e3e5656afdaba95bc7107ed76a35be04cf1d66a
Hans <105138715+HansDMendez@users.noreply.github.com>
2022-07-01 20:59:34 -0500

    Add files via upload

--- js/formulario.js
+++ js/formulario.js
@@ -89,7 +89,7 @@ formulario.addEventListener('submit', (e)=>{
     
 
     const terminos = document.getElementById('terminos');
-    if (campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked){
+    if (campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono){
         formulario.reset();
 
         document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
